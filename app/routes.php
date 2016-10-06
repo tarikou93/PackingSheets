999c69c87cf9117a28eedffedd725dae2160ad08
FranckParis <franck.paris7@gmail.com>
2016-10-06 11:10:08 +0200

    Detailed view completed, Data calculations completed

--- app/routes.php
+++ app/routes.php
@@ -8,7 +8,7 @@ $app->get('/', function () use ($app) {
 // PackingSheets list page
 $app->get('/sheets', function () use ($app) {
   $packingSheets = $app['dao.packingSheet']->findAll();
-  return $app['twig']->render('packingsheet.html.twig', array('packingSheets' => $packingSheets));
+  return $app['twig']->render('packingSheet.html.twig', array('packingSheets' => $packingSheets));
 })->bind('sheets');
 
 // Parts list page
@@ -17,19 +17,11 @@ $app->get('/parts', function () use ($app) {
     return $app['twig']->render('parts.html.twig', array('parts' => $parts));
 })->bind('parts');
 
-// PackingSheet details with packings
+// PackingSheet details with Packings and detailed parts
 $app->get('/sheets/{id}', function ($id) use ($app) {
     $packingSheet = $app['dao.packingSheet']->find($id);
     $packings = $app['dao.packing']->findAllByPackingSheet($id);
-    return $app['twig']->render('packingSheetDetails.html.twig', array('packingSheet' => $packingSheet, 'packings' => $packings));
-})->bind('sheetDetails');
-
-// PackingSheet details with Packings and detailed parts
-$app->get('/sheets/{idSheet}/{idPack}', function ($idSheet, $idPack) use ($app) {
-    $packingSheet = $app['dao.packingSheet']->find($idSheet);
-    $packings = $app['dao.packing']->findAllByPackingSheet($idSheet);
-    $packingParts = $app['dao.packingPart']->findAllByPacking($idPack);
+    $packingParts = $app['dao.packingPart']->findAll();
     $parts = $app['dao.part']->findAll();
-    $idPacking = $idPack;
-    return $app['twig']->render('packingSheetDetailsParts.html.twig', array('packingSheet' => $packingSheet, 'packings' => $packings, 'packingParts'=> $packingParts, 'parts'=>$parts, 'idPacking'=>$idPacking));
-})->bind('sheetDetailsParts');
+    return $app['twig']->render('packingSheetDetails.html.twig', array('packingSheet' => $packingSheet, 'packings' => $packings, 'packingParts'=> $packingParts, 'parts'=>$parts));
+})->bind('sheetDetails');
